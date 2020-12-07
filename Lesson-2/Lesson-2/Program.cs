b388d84f7b06688ac6275632fceeeff954bb35ce
Dmitriy <oddff@mail.ru>
2020-12-07 20:45:04 +0300

    Задание 1

---
+++ Lesson-2/Lesson-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Задание №1");
+
+            Console.Write("Введите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите третье число: ");
+            int c = int.Parse(Console.ReadLine());
+            NewMethod(a, b, c);
+        }
+
+        private static void NewMethod(int a, int b, int c)
+        {
+            if (a < c && a < b)
+                Console.WriteLine($"Минимальное число {a}");
+            else if (b < c)
+                Console.WriteLine($"Минимальное число {b}");
+            else
+                Console.WriteLine($"Минимальное число {c}");
+        }
+    }
+    }
+}
