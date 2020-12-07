9645b6d68b89461799c62937bd878208d573819d
Dmitriy <oddff@mail.ru>
2020-12-07 23:18:47 +0300

    Задание 3

---
+++ Lesson-2/task-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+            Console.WriteLine("Введите целое число: ");
+            do
+            {
+                while (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Введите целое число!");
+                }
+                if (number >= 0 && (number % 2 != 0))
+                {
+                    sum += number;
+                    Console.WriteLine($"Сумма нечетных положительных чисел = {sum}");
+                }
+            } while (number != 0);
+            Console.WriteLine($"Сумма нечетных положительных чисел = {sum}");
+        }
+    }
+}
