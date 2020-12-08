fdc800e4ed1363ecffec445ed0bc809493e15e61
Dmitriy <oddff@mail.ru>
2020-12-08 22:43:37 +0300

    Задание 5 б

--- Lesson-2/task-5/Program.cs
+++ Lesson-2/task-5/Program.cs
@@ -12,24 +12,66 @@ namespace task5
             float height = float.Parse(Console.ReadLine());
             height /= 100;
             Console.Write("Введите ваш вес: ");
-            float weight = float.Parse(Console.ReadLine());
+            double weight = double.Parse(Console.ReadLine());
 
-            float BMI = weight / (height * height);  //Вычесляем индекс массы тела
+            double BMI = weight / (height * height);  //Вычесляем индекс массы тела
+
+            double weightD = 0;
 
             if (BMI <= 15.99)
+            {
                 Console.WriteLine("Выраженный дефецит массы тела. Вам нужно набрать вес.");
-            else if(BMI >= 16 && BMI <= 18.49)
+                weightD = 20 * (height * height);
+                weight = weightD - weight;
+                Console.WriteLine("Вам нужно набрать вес на {0:f0} кг.", weight);
+            }
+
+            else if (BMI >= 16 && BMI <= 18.49)
+            {
                 Console.WriteLine("Недостаточная масса тела. Вам нужно набрать вес.");
+                weightD = 20 * (height * height);
+                weight = weightD - weight;
+                Console.WriteLine("Вам нужно набрать вес на {0:f0} кг.", weight);
+
+            }
+                
             else if (BMI >= 18.50 && BMI <= 24.99)
+            {
                 Console.WriteLine("Масса тела в норме.");
+            }
+                
             else if (BMI >= 25 && BMI <= 29.99)
+            {
                 Console.WriteLine("Избыточная масса тела. Вам нужно сбросить вес.");
+                weightD = 20 * (height * height);
+                weight -= weightD;
+                Console.WriteLine("Вам нужно сбросить вес на {0:f0} кг.", weight);
+            }
+                
             else if (BMI >= 30 && BMI <= 34.99)
+            {
                 Console.WriteLine("Ожирение первой степени. Вам нужно сбросить вес.");
+                weightD = 20 * (height * height);
+                weight -= weightD;
+                Console.WriteLine("Вам нужно сбросить вес на {0:f0} кг.", weight);
+            }
+                
             else if (BMI >= 35 && BMI <= 39.99)
+            {
                 Console.WriteLine("Ожирение второй степени. Вам нужно сбросить вес.");
+                weightD = 20 * (height * height);
+                weight -= weightD;
+                Console.WriteLine("Вам нужно сбросить вес на {0:f0} кг.", weight);
+            }
+                
             else
+            {
                 Console.WriteLine("Ожирение третьей степени. Вам нужно сбросить вес.");
+                weightD = 20 * (height * height);
+                weight -= weightD;
+                Console.WriteLine("Вам нужно сбросить вес на {0:f0} кг.", weight);
+            }    
+                
         }
     }
 }
