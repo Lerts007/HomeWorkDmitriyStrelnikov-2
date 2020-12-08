2bd90dac70bbef336babec124ff5c39cab8c9a0e
Dmitriy <oddff@mail.ru>
2020-12-08 20:41:12 +0300

    Задание 4

---
+++ Lesson-2/task-4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i = 3;
+            string login;
+            string password;
+            do
+            {
+                i--;
+                Console.Write("Введите логин: ");
+                login = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                password = Console.ReadLine();
+
+                i = NewMethod(i, login, password);
+
+
+            } while (i >= 0);
+
+        }
+
+        private static int NewMethod(int i, string login, string password)
+        {
+            if (login == "root" && password == "GeekBrains")
+            {
+                i = -1;
+
+            }
+
+            if (i == -1)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Да ты хакер!");
+                Console.ResetColor();
+            }
+            else
+                Console.WriteLine($"У вас осталось {i} попыток");
+            return i;
+        }
+    }
+}
