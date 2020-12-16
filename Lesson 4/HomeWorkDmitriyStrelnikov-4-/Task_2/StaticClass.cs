2912374979fb5251bd14fd4d85509c4c20eebf03
Dmitriy <oddff@mail.ru>
2020-12-16 22:14:59 +0300

    Задание 2

---
+++ Lesson 4/HomeWorkDmitriyStrelnikov-4-/Task_2/StaticClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Task_2
+{
+    public class StaticClass
+    {
+        private static int[] Start(int n)
+        {
+            int[] Arr = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                Arr[i] = r.Next(-100, 101);
+            }
+            return Arr;
+        }
+        public static void Enter(int n)
+        {
+            int[] Arr = Start(n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(Arr[i]);
+            }
+
+            Count(n, Arr);
+        }
+        static void Count(int n, int[] Arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if ((Arr[i] % 3 == 0) && (Arr[i + 1] % 3 != 0) || (Arr[i] % 3 != 0) && (Arr[i + 1] % 3 == 0))
+                    sum++;
+            }
+
+            Console.WriteLine($"Количество пар элемента массива где только один элемент делится на 3 = {sum}");
+            Console.ReadLine();
+        }
+    }
+}
