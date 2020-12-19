60cc8333e4dc1c22aa56b78206a0e81f578347fd
Dmitriy <oddff@mail.ru>
2020-12-19 12:52:12 +0300

    Урок 5 Задание 1

---
+++ HomeWorldDmitriyStrelnikov/Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = new Start();
+            a.Chek();
+        }
+    }
+}
