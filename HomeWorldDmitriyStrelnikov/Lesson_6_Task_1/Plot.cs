396b2562d8d0f0ef46681fb985ac412861199a45
Dmitriy <oddff@mail.ru>
2020-12-23 22:57:29 +0300

    Урок 6. Задание 1

---
+++ HomeWorldDmitriyStrelnikov/Lesson_6_Task_1/Plot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Lesson_6_Task_1
+{
+    public delegate double Function(double arg);
+
+    public class Plot
+    {
+        public Plot()
+        {
+        }
+        public double f(double x) { return 2 * x * x; }
+
+        public double g(double x) { return 2 * Math.Sin(x); }
+
+        public void Enter(Function fanc, int min, int max)
+        {
+            for (int x = min; x <= max; x++)
+            {
+                Console.WriteLine($"{x,3} | {fanc(x): 0.00}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HomeWorldDmitriyStrelnikov/Lesson_6_Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_6_Task_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = new Plot();
+            a.Enter(a.f, -2, 2);
+            a.Enter(a.g, -2, 2);
+        }
+    }
+}
